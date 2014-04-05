0987ebb5604dc4d62aecd81d0e5274439fd077a3
Armand <armand.dossantos@gmail.com>
2014-04-05 16:25:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Majgo::Application.config.secret_key_base = '9f571d16ed56a2b1fcf3afcf285e11f478fe05a4581f4847317146b0f00953c5887ada272f11c7898f6a855726b9675ed1637ac2daaf87f9f10b093fe742b9bc'
